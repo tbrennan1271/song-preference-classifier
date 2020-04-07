45cee10ff1c2a012f708169ac3283b013a891eb0
Tyler Brennan <58308659+tbrennan1271@users.noreply.github.com>
2020-04-07 14:14:52 -0400

    information_gain function
    Calculates the information gain for each attribute

--- song_attributes.py
+++ song_attributes.py
@@ -9,10 +9,14 @@ Created on Sat Apr  4 17:38:22 2020
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import tree
+import pandas as pd
 
 PLAYLIST = 'Music.csv'
 CURRENT_TOP = 'Top_Playlist.csv' 
 LABEL = 'LIKE'
+THRESHOLD_LIM = 5       # The number of sections the attributes will be divided into for the information gain
+TOP_N = 3               # Top n information gains to use in decision tree
 
 # In: name: file name  &  label: True if the data contains labels for data, and False if regular data
 # Out: data: array that contains all the data
@@ -45,7 +49,6 @@ def att_without_str(data, attribute):
     for att in attributes:
         if att != LABEL and (type(data[att][0]) == float or type(data[att][0]) == int):
                 att_no_str.append(att)
-    print(att_no_str)
     return att_no_str
 
 # Puts all the points within the range of 0 and 1
@@ -78,6 +81,49 @@ def create_histograms(data, data_pop, attributes):
         plt.show()
         print('My avg', att, ':', np.average(temp))
         print('Top avg', att, ':', np.average(temp))
+            
+def create_groups(data, attributes):
+    label_groups = {}
+    group = {}
+    threshold = 1/THRESHOLD_LIM
+    for att in attributes:
+        label_groups[att] = {}
+        group[att] = {}
+        for i in range(THRESHOLD_LIM):
+            val = (i + 1) * threshold
+            label_groups[att][val] = []
+            group[att][val] = [0, 0]        # [deximal value of liked points,  overall weight decimal value]
+        for i in range(len(data[att])):
+            pt = data[att][i]
+            pt_label = data[LABEL][i]
+            if pt == 0:
+                if pt_label == 1:
+                    group[att][threshold][0] += 1
+                group[att][threshold][1] += 1
+            for val in label_groups[att]:
+                if pt <= val and pt > (val - threshold):
+                    group[att][val][1] += 1
+                    if pt_label == 1:
+                        group[att][val][0] += 1
+        for val in label_groups[att]:
+            group[att][val][1] /= len(data[LABEL])
+            group[att][val][0] /= len(data[att])
+    return group
+
+def information_gain(groups, attributes):
+    ig = {}
+    for att in attributes:
+        final = 0
+        for pt in groups[att]:
+            entropy = 0
+            val = groups[att][pt][0]
+            weight = groups[att][pt][1]
+            if val != 0:
+                entropy -= (val * np.log2(val))
+                entropy -= ((1 - val) * np.log2(1 - val))
+            final += entropy * weight
+        ig[att] = final
+    return ig
 
 
 data_pop, attributes = get_data(CURRENT_TOP)
@@ -86,3 +132,9 @@ int_att = att_without_str(data, attributes)
 data = standardize(data, int_att)
 data_pop = standardize(data_pop, int_att)
 create_histograms(data, data_pop, int_att)
+groups = create_groups(data, int_att)
+info_gain = information_gain(groups, int_att)
+print(info_gain)
+
+
+
